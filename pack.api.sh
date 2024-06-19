dcdf862cf1287c8923bd3511b50e630c6a61295e
youngzhenhao <2499342961@qq.com>
2024-06-19 16:16:52 +0800

    add sh

---
+++ pack.api.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+currentPath=$(pwd)
+currentFolderName="${currentPath##*/}"
+specificString="wallet"
+
+if [ "$currentFolderName" == "$specificString" ]; then
+    echo "gomobile is in progress, please wait..."
+    # shellcheck disable=SC2164
+    cd api
+    gomobile bind -target android
+    # shellcheck disable=SC2103
+    cd ..
+    end_time=$(date +%s)
+    time_taken=$((end_time - start_time))
+    echo "Time cost: $time_taken seconds."
+else
+    echo "Wrong current directory, please run script in wallet."
+    # shellcheck disable=SC2162
+    read -p "Press enter to continue"
+fi
