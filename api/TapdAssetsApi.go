c76d4c68f79829fdedaa318a4a01e946b08ce745
youngzhenhao <2499342961@qq.com>
2024-07-19 13:39:30 +0800

    fix: asset holder nil pointer error

--- api/TapdAssetsApi.go
+++ api/TapdAssetsApi.go
@@ -3426,16 +3426,17 @@ func ListBalancesAndProcess() (*[]ListBalanceInfo, error) {
 
 type AssetBalanceInfo struct {
 	gorm.Model
-	GenesisPoint string `json:"genesis_point"`
-	Name         string `json:"name"`
-	MetaHash     string `json:"meta_hash"`
-	AssetID      string `json:"asset_id"`
-	AssetType    string `json:"asset_type"`
+	GenesisPoint string `json:"genesis_point" gorm:"type:varchar(255)"`
+	Name         string `json:"name" gorm:"type:varchar(255)"`
+	MetaHash     string `json:"meta_hash" gorm:"type:varchar(255)"`
+	AssetID      string `json:"asset_id" gorm:"type:varchar(255)"`
+	AssetType    string `json:"asset_type" gorm:"type:varchar(255)"`
 	OutputIndex  int    `json:"output_index"`
 	Version      int    `json:"version"`
 	Balance      int    `json:"balance"`
 	DeviceId     string `json:"device_id" gorm:"type:varchar(255)"`
 	UserId       int    `json:"user_id"`
+	Username     string `json:"username" gorm:"type:varchar(255)"`
 	Status       int    `json:"status" gorm:"default:1"`
 }
 
@@ -3874,8 +3875,9 @@ func GetAssetHolderBalanceByAssetBalancesInfo(token string, assetId string) (*As
 type AssetBalanceInfoSimplified struct {
 	Version  int    `json:"version"`
 	Balance  int    `json:"balance"`
-	DeviceId string `json:"device_id" gorm:"type:varchar(255)"`
+	DeviceId string `json:"device_id"`
 	UserId   int    `json:"user_id"`
+	Username string `json:"username"`
 }
 
 type AssetIdAndBalanceSimplified struct {
@@ -3890,12 +3892,16 @@ func AssetIdAndBalanceToAssetIdAndBalanceSimplified(assetIdAndBalance *AssetIdAn
 	assetIdAndBalanceSimplified := &AssetIdAndBalanceSimplified{}
 	assetIdAndBalanceSimplified.AssetId = assetIdAndBalance.AssetId
 	var assetBalanceInfoSimplified []AssetBalanceInfoSimplified
+	if assetIdAndBalance.AssetBalances == nil {
+		return assetIdAndBalanceSimplified
+	}
 	for _, assetBalanceInfo := range *(assetIdAndBalance.AssetBalances) {
 		assetBalanceInfoSimplified = append(assetBalanceInfoSimplified, AssetBalanceInfoSimplified{
 			Version:  assetBalanceInfo.Version,
 			Balance:  assetBalanceInfo.Balance,
 			DeviceId: assetBalanceInfo.DeviceId,
 			UserId:   assetBalanceInfo.UserId,
+			Username: assetBalanceInfo.Username,
 		})
 	}
 	assetIdAndBalanceSimplified.AssetBalances = &assetBalanceInfoSimplified
