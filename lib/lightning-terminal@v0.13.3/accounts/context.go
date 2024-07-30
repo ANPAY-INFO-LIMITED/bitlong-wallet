c0f57dad7e33c0ccba8cfb26573094c62cad7a85
SevenLiquor <1013871560@qq.com>
2024-07-30 14:25:17 +0800

    update: 1.依赖包更新,litd:0.13.3,lnd:0.18.2,tapd:0.4.1,解决了部分冲突 2.优化了依赖包中设置默认包启动路径，在源码中删除部分冗余的依赖库。 todo：tapd包修改的部分尚未兼容，待运行测试稳定后集成 new：编译，打包需加上 -tags "signrpc walletrpc chainrpc invoicesrpc autopilotrpc btlapi"

---
+++ lib/lightning-terminal@v0.13.3/accounts/context.go
@@ -0,0 +1,99 @@
+package accounts
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/btcsuite/btclog"
+	"github.com/lightningnetwork/lnd/build"
+)
+
+// ContextKey is the type that we use to identify account specific values in the
+// request context. We wrap the string inside a struct because of this comment
+// in the context API: "The provided key must be comparable and should not be of
+// type string or any other built-in type to avoid collisions between packages
+// using context."
+type ContextKey struct {
+	Name string
+}
+
+var (
+	// KeyAccount is the key under which we store the account in the request
+	// context.
+	KeyAccount = ContextKey{"account"}
+
+	// KeyRequestID is the key under which we store the middleware request
+	// ID.
+	KeyRequestID = ContextKey{"request_id"}
+)
+
+// FromContext tries to extract a value from the given context.
+func FromContext(ctx context.Context, key ContextKey) interface{} {
+	return ctx.Value(key)
+}
+
+// AddAccountToContext adds the given value to the context for easy retrieval
+// later on.
+func AddAccountToContext(ctx context.Context,
+	value *OffChainBalanceAccount) context.Context {
+
+	return context.WithValue(ctx, KeyAccount, value)
+}
+
+// AccountFromContext attempts to extract an account from the given context.
+func AccountFromContext(ctx context.Context) (*OffChainBalanceAccount, error) {
+	val := FromContext(ctx, KeyAccount)
+	if val == nil {
+		return nil, fmt.Errorf("no account found in context")
+	}
+
+	acct, ok := val.(*OffChainBalanceAccount)
+	if !ok {
+		return nil, fmt.Errorf("invalid account value in context")
+	}
+
+	return acct, nil
+}
+
+// AddRequestIDToContext adds the given request ID to the context for easy
+// retrieval later on.
+func AddRequestIDToContext(ctx context.Context, value uint64) context.Context {
+	return context.WithValue(ctx, KeyRequestID, value)
+}
+
+// RequestIDFromContext attempts to extract a request ID from the given context.
+func RequestIDFromContext(ctx context.Context) (uint64, error) {
+	val := FromContext(ctx, KeyRequestID)
+	if val == nil {
+		return 0, fmt.Errorf("no request ID found in context")
+	}
+
+	reqID, ok := val.(uint64)
+	if !ok {
+		return 0, fmt.Errorf("invalid request ID value in context")
+	}
+
+	return reqID, nil
+}
+
+// requestScopedValuesFromCtx is a helper function that can be used to extract
+// an account and requestID from the given context. It also creates a new
+// prefixed logger that can be used by account request and response handlers.
+// Each log line will be prefixed by the account ID and the request ID.
+func requestScopedValuesFromCtx(ctx context.Context) (btclog.Logger,
+	*OffChainBalanceAccount, uint64, error) {
+
+	acc, err := AccountFromContext(ctx)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+
+	reqID, err := RequestIDFromContext(ctx)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+
+	prefix := fmt.Sprintf("[account: %s, request: %d]", acc.ID, reqID)
+
+	return build.NewPrefixLog(prefix, log), acc, reqID, nil
+}
---
+++ lib/lightning-terminal@v0.13.3/status/manager.go
@@ -0,0 +1,273 @@
+package status
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/lightninglabs/lightning-terminal/litrpc"
+)
+
+// SubServerOption defines a functional option that can be used to modify the
+// values of a subServer's fields.
+type SubServerOption func(status *subServer)
+
+// WithIsReadyOverride is a functional option that can be used to set a callback
+// function that is used to check if a system is ready _iff_ the system running
+// status is not yet true. The call-back will be passed the request URI along
+// with any manual status that has been set for the subsystem.
+func WithIsReadyOverride(fn func(string, string) (bool, bool)) SubServerOption {
+	return func(status *subServer) {
+		status.isReadyOverride = fn
+	}
+}
+
+// subServer represents the status of a sub-server.
+type subServer struct {
+	// disabled is true if the sub-server is available in the LiT bundle but
+	// has explicitly been disabled by the user.
+	disabled bool
+
+	// running is true if the sub-server is enabled and has successfully
+	// been started.
+	running bool
+
+	// customStatus is a string that details a custom status of the
+	// sub-server, if the the sub-server is in a custom state. This status
+	// can be set to a unique status that only exists for the specific
+	// sub-server, and will be displayed to the user with the
+	// litrpc.SubServerStatus.
+	customStatus string
+
+	// err will be a non-empty string if the sub-server failed to start.
+	err string
+
+	// isReadyOverride is a call back that, when set and only if `running`
+	// is not yet true, will be used to determine if a system is ready for
+	// a call. We will pass the request URI to this method along with the
+	// `manualStatus`. The first returned boolean is true if the system
+	// should be seen as ready and the second is true if the override does
+	// handle the given request. If it does not, then we will fall back to
+	// our normal is-ready check.
+	isReadyOverride func(string, string) (bool, bool)
+}
+
+// newSubServer constructs a new subServer.
+func newSubServer(disabled bool, opts ...SubServerOption) *subServer {
+	s := &subServer{
+		disabled: disabled,
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	return s
+}
+
+// Manager manages the status of any sub-server registered to it. It is also an
+// implementation of the litrpc.StatusServer which can be queried for the status
+// of various LiT sub-servers.
+type Manager struct {
+	litrpc.UnimplementedStatusServer
+
+	subServers map[string]*subServer
+	mu         sync.RWMutex
+}
+
+// NewStatusManager constructs a new Manager.
+func NewStatusManager() *Manager {
+	return &Manager{
+		subServers: make(map[string]*subServer),
+	}
+}
+
+// IsSystemReady shows if the given sub-server ready to handle the a request for
+// the passed request URI. The first returned boolean is true if the system
+// is ready to handle the request. The second returned boolean is true if the
+// system has been disabled.
+func (s *Manager) IsSystemReady(name, req string) (bool, bool, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	server, ok := s.subServers[name]
+	if !ok {
+		return false, false, errors.New("a sub-server with " +
+			"name %s has not yet been registered")
+	}
+
+	if server.disabled {
+		return false, true, nil
+	}
+
+	// If there is no override for this server or if the server is already
+	// running then we just return the 'running' status.
+	if server.isReadyOverride == nil || server.running {
+		return server.running, false, nil
+	}
+
+	// Otherwise, we check the override to see if this request is handled
+	// by the override and if it is, then if the override permits this call.
+	isReady, handled := server.isReadyOverride(req, server.customStatus)
+	if handled {
+		return isReady, false, nil
+	}
+
+	// Otherwise, we just return the running status.
+	return server.running, false, nil
+}
+
+// SubServerStatus queries the current status of a given sub-server.
+//
+// NOTE: this is part of the litrpc.StatusServer interface.
+func (s *Manager) SubServerStatus(_ context.Context,
+	_ *litrpc.SubServerStatusReq) (*litrpc.SubServerStatusResp,
+	error) {
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	resp := make(map[string]*litrpc.SubServerStatus, len(s.subServers))
+	for server, status := range s.subServers {
+		resp[server] = &litrpc.SubServerStatus{
+			Disabled:     status.disabled,
+			Running:      status.running,
+			Error:        status.err,
+			CustomStatus: status.customStatus,
+		}
+	}
+
+	return &litrpc.SubServerStatusResp{
+		SubServers: resp,
+	}, nil
+}
+
+// RegisterSubServer will create a new sub-server entry for the Manager to
+// keep track of.
+func (s *Manager) RegisterSubServer(name string, opts ...SubServerOption) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	s.registerSubServerUnsafe(name, true, opts...)
+}
+
+// RegisterAndEnableSubServer will create a new sub-server entry for the
+// Manager to keep track of and will set it as enabled.
+func (s *Manager) RegisterAndEnableSubServer(name string,
+	opts ...SubServerOption) {
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	s.registerSubServerUnsafe(name, false, opts...)
+}
+
+func (s *Manager) registerSubServerUnsafe(name string, disabled bool,
+	opts ...SubServerOption) {
+
+	ss := newSubServer(disabled, opts...)
+
+	s.subServers[name] = ss
+}
+
+// SetCustomStatus updates the custom status of the given sub-server to the
+// passed status.
+func (s *Manager) SetCustomStatus(name, customStatus string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	ss, ok := s.subServers[name]
+	if !ok {
+		return
+	}
+
+	ss.customStatus = customStatus
+}
+
+// SetEnabled marks the sub-server with the given name as enabled.
+//
+// NOTE: This will silently fail if the referenced sub-server has not yet been
+// registered.
+func (s *Manager) SetEnabled(name string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	log.Debugf("Setting the %s sub-server as enabled", name)
+
+	ss, ok := s.subServers[name]
+	if !ok {
+		return
+	}
+
+	ss.disabled = false
+}
+
+// SetRunning can be used to set the status of a sub-server as Running
+// with no errors.
+//
+// NOTE: This will silently fail if the referenced sub-server has not yet been
+// registered.
+func (s *Manager) SetRunning(name string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	log.Debugf("Setting the %s sub-server as running", name)
+
+	ss, ok := s.subServers[name]
+	if !ok {
+		return
+	}
+
+	ss.running = true
+	ss.err = ""
+	ss.customStatus = ""
+}
+
+// SetStopped can be used to set the status of a sub-server as not Running and
+// with no errors.
+//
+// NOTE: This will silently fail if the referenced sub-server has not yet been
+// registered.
+func (s *Manager) SetStopped(name string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	log.Debugf("Setting the %s sub-server as stopped", name)
+
+	ss, ok := s.subServers[name]
+	if !ok {
+		return
+	}
+
+	ss.running = false
+	ss.err = ""
+	ss.customStatus = ""
+}
+
+// SetErrored can be used to set the status of a sub-server as not Running
+// and also to set an error message for the sub-server.
+//
+// NOTE: This will silently fail if the referenced sub-server has not yet been
+// registered.
+func (s *Manager) SetErrored(name string, errStr string,
+	params ...interface{}) {
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	err := fmt.Sprintf(errStr, params...)
+
+	log.Debugf("Setting the %s sub-server as errored: %s", name, err)
+
+	ss, ok := s.subServers[name]
+	if !ok {
+		return
+	}
+
+	log.Errorf("could not start the %s sub-server: %s", name, err)
+
+	ss.running = false
+	ss.err = err
+	ss.customStatus = ""
+}
