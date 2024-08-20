927343a1115306372b405c3d4202a3339b667fda
SevenLiquor <1013871560@qq.com>
2024-08-20 11:24:20 +0800

    feat：增加查询的宇宙outpoint的数量限制为5120 fix: visoin：0。0.7

--- api/ApiVersion.go
+++ api/ApiVersion.go
@@ -3,12 +3,12 @@ package api
 const (
 	PrimaryVersion = "0"
 	SubVersion     = "0"
-	DevVersion     = "6"
+	DevVersion     = "7"
 )
 
 const (
 	BaseVersion = "v" + PrimaryVersion + "." + SubVersion + "." + DevVersion
-	DateTime    = "20240809111710"
+	DateTime    = "2024-8-20 11:23:41"
 )
 
 func apiVersionWithMaker(maker string) string {
