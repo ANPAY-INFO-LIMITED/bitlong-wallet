c345306fc42ddc73984b528172f274d34fd43bc6
youngzhenhao <2499342961@qq.com>
2024-10-11 14:59:04 +0800

    update

--- api/Schnorr.go
+++ api/Schnorr.go
@@ -1,12 +1,31 @@
 package api
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/decred/dcrd/crypto/blake256"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4/schnorr"
 	_ "golang.org/x/crypto/sha3"
 )
+
+// SignSchnorr
+// @Description: ONLY USE THIS NOW
+func SignSchnorr(hexPrivateKey string, message string) (string, error) {
+	s, err := hex.DecodeString(hexPrivateKey)
+	if err != nil {
+		return "", fmt.Errorf("Sign called with invalid private key '%s': %w", hexPrivateKey, err)
+	}
+	sk, _ := btcec.PrivKeyFromBytes(s)
+	h := sha256.Sum256([]byte(message))
+	sig, err := schnorr.Sign(sk, h[:])
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(sig.Serialize()), nil
+}
 
 func Sign(privKey string, msg string) (sigHex string, err error) {
 	// Decode a hex-encoded private key
