bf4b49ac5d3b17e6e3991e31a1bb10a300d00d01
SevenLiquor <1013871560@qq.com>
2025-01-13 15:51:53 +0800

    vision: 0.1.52 feat：更新通道资产排行接口 fix:

--- api/ApiVersion.go
+++ api/ApiVersion.go
@@ -3,13 +3,13 @@ package api
 const (
 	PrimaryVersion = "0"
 	SubVersion     = "1"
-	DevVersion     = "51"
+	DevVersion     = "52"
 )
 
 const (
 	BaseVersion = "v" + PrimaryVersion + "." + SubVersion + "." + DevVersion
 	// GetNowTimeStringWithHyphens
-	DateTime = "2025-1-2 10:59:51"
+	DateTime = "2025-1-13 15:51:39"
 )
 
 func apiVersionWithMaker(maker string) string {
