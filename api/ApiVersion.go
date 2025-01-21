014c2075175e2392bde896ce3b1ae7354718a4b3
SevenLiquor <1013871560@qq.com>
2025-01-21 09:52:45 +0800

    vision: 0.1.55 feat：修复证明文件查询问题 fix:

--- api/ApiVersion.go
+++ api/ApiVersion.go
@@ -3,13 +3,13 @@ package api
 const (
 	PrimaryVersion = "0"
 	SubVersion     = "1"
-	DevVersion     = "54"
+	DevVersion     = "55"
 )
 
 const (
 	BaseVersion = "v" + PrimaryVersion + "." + SubVersion + "." + DevVersion
 	// GetNowTimeStringWithHyphens
-	DateTime = "2025-1-20 17:25:01"
+	DateTime = "2025-1-21 09:52:11"
 )
 
 func apiVersionWithMaker(maker string) string {
